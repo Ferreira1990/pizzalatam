92b02008d5c2e46c3390c245e58b634964e10758
Cristian Ferreira <ferreira.8390@gmail.com>
2023-10-28 14:42:18 -0300

    deletesrc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import '../components/styles/Navbar.css'
 import { Link } from 'react-router-dom'
 import Products from './Products'
+import Logopizza from '../assets/Logo.png'
+import Customcarritox from '../assets/Carrito.png'
+import Bannerx from '../assets/PizzaBanner.jpg'
 
 
 
@@ -9,12 +12,12 @@ const Navbar = () => {
   return (
     <div className='CustomnavPadre'>
     <div className='Customnav'>
-        <Link to={"/"} className='Customspacegreen'><img className='Customlogo' src="src\assets\Logo.png" alt="Logo pizza" /> </Link>
+        <Link to={"/"} className='Customspacegreen'><img className='Customlogo' src={Logopizza} alt="Logo pizza" /> </Link>
         <div className='Customspace'></div>
-        <Link to={"/car"} className='Customspacered'><img className='Customcarrito' src="src\assets\Carrito.png" alt="Imagen de carrito" /></Link>
+        <Link to={"/car"} className='Customspacered'><img className='Customcarrito' src={Customcarritox} alt="Imagen de carrito" /></Link>
     </div>
     <div>
-        <img className='Bannercustom' src="src\assets\PizzaBanner.jpg" alt="Banner Pizza" />
+        <img className='Bannercustom' src={Bannerx} alt="Banner Pizza" />
         <div className='CardContainer'>
         <Products />
         </div>
